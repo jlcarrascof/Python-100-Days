e8aae85411400c5f861138f158f3dc9f07f19fec
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-13 12:08:58 -0400

    feat: Add code to calculate the length of a name

---
+++ Day-001/variables.py
@@ -0,0 +1,13 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# First version
+print( len ( input ( "What is your name? " ) ) )
+
+# Second version
+name = input("What is your name? ")
+length = len(name)
+print(length)
