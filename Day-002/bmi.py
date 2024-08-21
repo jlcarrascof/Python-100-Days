dfcd5c36420904c1dc095c3b857c44f48b07f9a4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-21 12:21:19 -0400

    feat: Add code to calculate BMI from height and weight inputs

---
+++ Day-002/bmi.py
@@ -0,0 +1,12 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+# My Solution
+height = float(height)
+weight = float(weight)
+BMI = (weight / height ** 2)
+print(BMI)
