d58754f2c36e1f75d2dce8ab010d4a2f1df3b277
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-21 11:28:20 -0400

    feat: Add code to extract and sum two digits from a two-digit number

---
+++ Day-002/dataTypes.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+# My Solution
+number1 = int(str(two_digit_number[0]))
+number2 = int(str(two_digit_number[1]))
+print(number1 + number2)
