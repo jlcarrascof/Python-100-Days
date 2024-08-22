fd373d641783fa33893d22e44fb3d1305fcbdb47
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-22 12:03:17 -0400

    feat: Add code to calculate bill split with tip

--- Day-002/final-project.py
+++ Day-002/final-project.py
@@ -6,3 +6,14 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+print("Welcome to the tip calculator")
+total = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+tip_percent = tip/100
+tip_percent	+= 1
+total_plus_tip = round(total * tip_percent, 2)
+amount_per_people = round(total_plus_tip / people, 2)
+
+print(f"Each person should pay: $ {amount_per_people}")
