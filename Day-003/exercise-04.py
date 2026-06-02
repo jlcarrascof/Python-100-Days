6dcfbe27298c2c6b2b1171b113df68eb0e165639
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-02 17:17:11 -0400

    feat: solve Leap Year Calculator (exercise-04) - nested if statements

--- Day-003/exercise-04.py
+++ Day-003/exercise-04.py
@@ -3,3 +3,13 @@ year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year")    
+    else:
+        print("Leap year.")        
+else:
+    print("Not leap year.")    
