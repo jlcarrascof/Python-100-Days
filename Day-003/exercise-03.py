256776424a788d992f803cdb2acf849fbd908127
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-02 12:52:10 -0400

    feat: solve BMI Calculator 2.0 (exercise-03) - Day 3 if/elif/else logic

--- Day-003/exercise-03.py
+++ Day-003/exercise-03.py
@@ -4,3 +4,16 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bmi = weight / height ** 2 
+
+if bmi < 18.5:
+    print('Underweight')
+elif bmi < 25:
+    print('Normal weight')
+elif bmi < 30:
+    print('Overweight')
+elif bmi < 35:
+    print('Obese')
+else:
+    print('Clinically Obese')
+
