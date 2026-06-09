f9961a2a31af774696bf9f1df1d398c5e51b19cd
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-08 21:56:15 -0400

    feat: solve Pizza Deliveries (exercise-05) - nested and successive conditionals

--- Day-003/exercise-05.py
+++ Day-003/exercise-05.py
@@ -1,8 +1,26 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N ").upper()
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is ${bill}")    
