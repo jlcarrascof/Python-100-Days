1140a766c8d59ca3d6c9b30a276172edc1defa08
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-09 22:30:43 -0400

    feat: solve Love Calculator (exercise-06) - add Angela's version as reference under her comment

--- Day-003/exercise-06.py
+++ Day-003/exercise-06.py
@@ -6,6 +6,8 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
+# Javier's Version 
+
 combined_names = name1 + name2
 combined_names = combined_names.lower()
 
@@ -28,7 +30,7 @@ love_count += combined_names.count("e")
 score_string = str(true_count) + str(love_count)
 love_score = int(score_string)
 
-# Angela's Version
+# Angela's version
 # combined_string = name1 + name2
 # lower_case_string = combined_string.lower()
 # t = lower_case_string.count("t")
@@ -42,8 +44,6 @@ love_score = int(score_string)
 # e = lower_case_string.count("e")
 # love = l + o + v + e
 # love_score = int(str(true) + str(love))
-
-
 if love_score < 10 or love_score > 90:
     print(f"Your score is {love_score}, you go together like coke and mentos.")
 elif love_score >= 40 and love_score <= 50:
