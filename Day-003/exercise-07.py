8940d7ff83c2dd67b9ff7940114b921fbbd04d4a
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-06-10 20:00:25 -0400

    feat: solve Treasure Island project (exercise-07) - nested conditionals

--- Day-003/exercise-07.py
+++ Day-003/exercise-07.py
@@ -25,4 +25,19 @@ print("Your mission is to find the treasure.")
 
 # Write your code below this line 👇
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
-
+if choice1 == "left":
+     choice2 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "Swim" to swim across\n').lower()
+     if choice2 == "wait": 
+          choice3 = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n').lower()
+          if choice3 == "yellow":
+               print("You Win!")
+          elif choice3 == "red":
+               print("Burned by fire.\nGAME OVER.")
+          elif choice3 == "blue":
+               print("Eaten by beasts.\nGAME OVER.")
+          else:
+               print("GAME OVER.")               
+     else:
+        print("Attacked by an angry trout.\nGAME OVER.")
+else:
+    print("Fall into a hole.\nGAME OVER.")        
